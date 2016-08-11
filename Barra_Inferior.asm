9962781563fcce842c4770a2e88edc172c3de8c9
Ccgenius <josegabrielca@gmail.com>
2016-08-10 18:48:28 -0600

    Se Agrega el control para mover la paleta,En este caso parece que solo lo va a hacer para arriba y para abajo, además, pareciera por los registros utilizados, que la arquitectura es de 16 bits, falta por compilar, error en líneas 40 y pico, que se puede arreglar porque parace ser del tipo de ensamblador utilizado

--- Barra_Inferior.asm
+++ Barra_Inferior.asm
@@ -25,118 +25,161 @@ segment .data
 ;---------------------Velocidad de los Objetos (Pixeles por tpoque)
 	PaddleSpeed equ 4
 
+;-----------------------Codigo--------------------------------
 
 section .text
 
-; /////////////////////////////////////////////////////////////////////
-; Subrutina que maneja el movimiento de las PALETAS
-PaddleMovement proc near
+Hline:
+	push bp
+	mov bp,sp
+	push ax
+	push bx
+	push cx
+	push di
+	push es
+	 
+	mov ax,8[bp] ; cambiar X1,X2 si X1>X2
+	cmp ax,6[bp]
+	JB OKh
+	mov bx,6[bp]
+	mov 6[bp],ax
+	mov 8[bp],bx
+	 
+OKh:
+	mov ax,4[bp] ; ax=y
+	xor ah,ah
+	xchg ah,al   ; ax=y*256
+	mov bx,ax    ; bx=y*256
+	shr bx,1
+	shr bx,1     ; bx =y*64
+	add ax,bx    ; ax=y*320
+	add ax,8[bp] ; offset de 1er punto
+	mov di,ax
+	mov ax, VideoSeg
+	mov es,ax
+	mov cx,6[bp]
+	sub cx,8[bp] ; cx = longitud de linea
+	inc cx
+	mov al,Fcolor
+	rep stosb
+	 
+	pop es
+	pop di
+	pop cx
+	pop bx
+	pop ax
+	pop bp
+	ret 6
  
-call ReadKeyboard ; Subrutina para leer buffer del teclado
-                  ; devuelve en AL el scan code de una tecla
+;------------------------ Subrutina que maneja el movimiento de las PALETAS-------------------------------------
+PaddleMovement:
  
-cmp al,0
-je exit_paddle_movement ; Si no se ha presionado nada no hay accion
+	call ReadKeyboard ; Subrutina para leer buffer del teclado
+   					  ; devuelve en AL el scan code de una tecla
  
-cmp al,72             ; Si el usuario presiona la tecla direccional arriba
-je MoveLeftPaddleUp   ; Mueve paleta izquiera hacia arriba
-cmp al,80             ; Si el usuario presiona la tecla direccional abajo
-je MoveLeftPaddleDown ; Mueve paleta izquiera hacia abajo
+	cmp al,0
+	je exit_paddle_movement ; Si no se ha presionado nada no hay accion
  
-cmp al,30             ; Si el usuario presiona la tecla A
-je MoveRightPaddleUp   ; Mueve paleta derecha hacia arriba
-cmp al,44             ; Si el usuario presiona la tecla Z
-je MoveRightPaddleDown ; Mueve paleta derecha hacia abajo
+	cmp al,72             ; Si el usuario presiona la tecla direccional arriba
+	je MoveLeftPaddleUp   ; Mueve paleta izquiera hacia arriba
+	
+	cmp al,80             ; Si el usuario presiona la tecla direccional abajo
+	je MoveLeftPaddleDown ; Mueve paleta izquiera hacia abajo
  
-jmp exit_paddle_movement
+	jmp exit_paddle_movement
  
 MoveLeftPaddleUp:
-mov cx,PaddleSpeed
+	mov cx,PaddleSpeed
  
 Loop_paddle_up:
  
-mov ax,LeftPaddle_x1 ; Carga coordenadas actuales de la paleta
-mov bx,LeftPaddle_x2
-mov dx,LeftPaddle_y1
-cmp dx,TopLimit      ; Si ya alcanzo el tope superior, entonces
-je exit_paddle_up    ; No realiza ningun movimiento mas
-dec dx
-mov LeftPaddle_y1,dx
+	mov ax,LeftPaddle_x1 ; Carga coordenadas actuales de la paleta
+	mov bx,LeftPaddle_x2
+	mov dx,LeftPaddle_y1
+	
+	cmp dx,TopLimit      ; Si ya alcanzo el tope superior, entonces
+	je exit_paddle_up    ; No realiza ningun movimiento mas
+
+	dec dx
+	mov LeftPaddle_y1,dx
  
-mov FColor,ColorPink
+	;mov FColor,ColorPink				;----------------colorean
  
-mov LoopCounter,cl
+	mov LoopCounter,cl
  
-push ax
-push bx
-push dx
-call Hline
+	push ax
+	push bx
+	push dx
+	call Hline
  
-mov ax,LeftPaddle_x1
-mov bx,LeftPaddle_x2
-mov dx,LeftPaddle_y2
+	mov ax,LeftPaddle_x1
+	mov bx,LeftPaddle_x2
+	mov dx,LeftPaddle_y2
  
-mov Fcolor,ColorBlack
+	;mov Fcolor,ColorBlack				;----------------colorean
  
-push ax
-push bx
-push dx
-call Hline
+	push ax
+	push bx
+	push dx
+	call Hline
  
-mov dx,LeftPaddle_y2
-dec dx
-mov LeftPaddle_y2,dx
+	mov dx,LeftPaddle_y2
+	dec dx
+	mov LeftPaddle_y2,dx
  
-mov ch,0
-mov cl,LoopCounter
-loop Loop_paddle_up
+	mov ch,0
+	mov cl,LoopCounter
+	loop Loop_paddle_up
  
 exit_paddle_up:
-jmp exit_paddle_movement
+	jmp exit_paddle_movement
  
 MoveLeftPaddleDown:
- 
-mov cx,PaddleSpeed
+	mov cx,PaddleSpeed
+
 Loop_paddle_down:
+	mov ax,LeftPaddle_x1
+	mov bx,LeftPaddle_x2
+	mov dx,LeftPaddle_y2
+	
+	cmp dx,BottomLimit
+	je exit_paddle_down
+	inc dx
+	mov LeftPaddle_y2,dx
  
-mov ax,LeftPaddle_x1
-mov bx,LeftPaddle_x2
-mov dx,LeftPaddle_y2
-cmp dx,BottomLimit
-je exit_paddle_down
-inc dx
-mov LeftPaddle_y2,dx
+	mov LoopCounter,cl
  
-mov LoopCounter,cl
+	;mov FColor,ColorPink				;--------colorea
  
-mov FColor,ColorPink
+	push ax
+	push bx
+	push dx
+	call Hline
  
-push ax
-push bx
-push dx
-call Hline
+	mov ax,LeftPaddle_x1
+	mov bx,LeftPaddle_x2
+	mov dx,LeftPaddle_y1
+	 
+	;mov Fcolor,ColorBlack				;-------cololea
  
-mov ax,LeftPaddle_x1
-mov bx,LeftPaddle_x2
-mov dx,LeftPaddle_y1
+	push ax
+	push bx
+	push dx
+	call Hline
  
-mov Fcolor,ColorBlack
+	mov dx,LeftPaddle_y1
+	inc dx
+	mov LeftPaddle_y1,dx
  
-push ax
-push bx
-push dx
-call Hline
- 
-mov dx,LeftPaddle_y1
-inc dx
-mov LeftPaddle_y1,dx
- 
-mov ch,0
-mov cl,LoopCounter
-loop Loop_paddle_down
+	mov ch,0
+	mov cl,LoopCounter
+	loop Loop_paddle_down
  
 exit_paddle_down:
-jmp exit_paddle_movement
+	jmp exit_paddle_movement
+
+exit_paddle_movement:
+	ret
 
 ReadKeyboard:
  
