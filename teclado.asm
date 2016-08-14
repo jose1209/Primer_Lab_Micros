ffd46c7e7eed61424b9bdc34ac7528356ad792eb
Allan Gerardo Carmona Cruz <agerardocarmona94@gmail.com>
2016-08-14 02:30:43 -0600

    actualizacion
    se agrega lectura de teclado y eliminacion de modo canonico y echo ALLAN

--- teclado.asm
+++ teclado.asm
@@ -15,6 +15,8 @@ timeval:
 
 p1 dq 0       ;plataforma 1 izquierda, 
 p2 dq 0		  ;plataforma 2 derecha
+tecla_derecha dq 0
+tecla_izquierda dq 0
 
 borra db 0x1b,"[2J"
 len2 equ $-borra
@@ -22,20 +24,244 @@ len2 equ $-borra
 barra db  0x1b,"[21;40f","********"
 len3 equ $-barra
 
+cursor db  0x1b,"[?25l"
+len4 equ $-cursor
+
+
+tecla dq 0
+
+variable: db''													;Almacenamiento de la tecla capturada
+
+termios:        times 36 db 0									;Estructura de 36bytes que contiene el modo de operacion de la consola
+stdin:          	  equ 0												;Standard Input (se usa stdin en lugar de escribir manualmente los valores)
+ICANON:      equ 1<<1											;ICANON: Valor de control para encender/apagar el modo canonico
+ECHO:           equ 1<<3    											;ECHO: Valor de control para encender/apagar el modo de eco
+
+
+canonical_off:
+
+	;Se llama a la funcion que lee el estado actual del TERMIOS en STDIN
+	;TERMIOS son los parametros de configuracion que usa Linux para STDIN
+        call read_stdin_termios
+
+	;Se escribe el nuevo valor de ICANON en EAX, para apagar el modo canonico
+        push rax
+        mov eax, ICANON
+        not eax
+        and [termios+12], eax
+        pop rax
+
+	;Se escribe la nueva configuracion de TERMIOS
+        call write_stdin_termios
+        ret
+        ;Final de la funcion
+;###################################################
+
+
+;####################################################
+;echo_off
+;Esta es una funcion que sirve para apagar el modo echo en Linux
+;Cuando el modo echo se apaga, Linux NO muestra en la pantalla la tecla que
+;se acaba de presionar.
+;
+;Para apagar el modo echo, simplemente use: call echo_off
+;###################################################
+echo_off:
+
+	;Se llama a la funcion que lee el estado actual del TERMIOS en STDIN
+	;TERMIOS son los parametros de configuracion que usa Linux para STDIN
+        call read_stdin_termios
+
+        ;Se escribe el nuevo valor de ECHO en EAX para apagar el echo
+        push rax
+        mov eax, ECHO
+        not eax
+        and [termios+12], eax
+        pop rax
+
+	;Se escribe la nueva configuracion de TERMIOS
+        call write_stdin_termios
+        ret
+        ;Final de la funcion
+;###################################################
+
+
+;####################################################
+;canonical_on
+;Esta es una funcion que sirve para encender el modo canonico en Linux
+;Cuando el modo canonico se enciende, Linux espera un ENTER para
+;procesar lo que se captura desde el teclado
+;
+;Para encender el modo canonico, simplemente use: call canonical_on
+;###################################################
+canonical_on:
+
+	;Se llama a la funcion que lee el estado actual del TERMIOS en STDIN
+	;TERMIOS son los parametros de configuracion que usa Linux para STDIN
+        call read_stdin_termios
+
+        ;Se escribe el nuevo valor de modo Canonico
+        or dword [termios+12], ICANON
+
+	;Se escribe la nueva configuracion de TERMIOS
+        call write_stdin_termios
+        ret
+        ;Final de la funcion
+;###################################################
+
+
+;####################################################
+;echo_on
+;Esta es una funcion que sirve para encender el echo en Linux
+;Cuando el echo se enciende, Linux muestra en la pantalla (stdout) cada tecla
+;que se recibe del teclado (stdin)
+;
+;Para encender el modo echo, simplemente use: call echo_on
+;###################################################
+echo_on:
+
+	;Se llama a la funcion que lee el estado actual del TERMIOS en STDIN
+	;TERMIOS son los parametros de configuracion que usa Linux para STDIN
+        call read_stdin_termios
+
+        ;Se escribe el nuevo valor de modo echo
+        or dword [termios+12], ECHO
+
+	;Se escribe la nueva configuracion de TERMIOS
+        call write_stdin_termios
+        ret
+        ;Final de la funcion
+;###################################################
+
+
+;####################################################
+;read_stdin_termios
+;Esta es una funcion que sirve para leer la configuracion actual del stdin o 
+;teclado directamente de Linux
+;Esta configuracion se conoce como TERMIOS (Terminal Input/Output Settings)
+;Los valores del stdin se cargan con EAX=36h y llamada a la interrupcion 80h
+;
+;Para utilizarlo, simplemente se usa: call read_stdin_termios
+;###################################################
+read_stdin_termios:
+        push rax
+        push rbx
+        push rcx
+        push rdx
+
+        mov eax, 36h
+        mov ebx, stdin
+        mov ecx, 5401h
+        mov edx, termios
+        int 80h
+
+        pop rdx
+        pop rcx
+        pop rbx
+        pop rax
+        ret
+        ;Final de la funcion
+;###################################################
+
+
+;####################################################
+;write_stdin_termios
+;Esta es una funcion que sirve para escribir la configuracion actual del stdin o 
+;teclado directamente de Linux
+;Esta configuracion se conoce como TERMIOS (Terminal Input/Output Settings)
+;Los valores del stdin se cargan con EAX=36h y llamada a la interrupcion 80h
+;
+;Para utilizarlo, simplemente se usa: call write_stdin_termios
+;###################################################
+write_stdin_termios:
+        push rax
+        push rbx
+        push rcx
+        push rdx
+
+        mov eax, 36h
+        mov ebx, stdin
+        mov ecx, 5402h
+        mov edx, termios
+        int 80h
+
+        pop rdx
+        pop rcx
+        pop rbx
+        pop rax
+        ret
+        ;Final de la funcion
+;###################################################
+
+
+
 section .text
 global _start
 
 ;---------------------------Principal-----------------------------------------
 _start:
-call rara 
+escribe cursor,len4
+;call leer_tecla
+call canonical_off
+call echo_off
 mov qword [p1], 0x36;p1
 mov qword [p2], 0x32;p2
+mov qword [tecla_derecha], 0x00
+mov qword [tecla_izquierda],0x00
+mov qword [tecla],0x00
 call limpiar_pantalla
-;call posicion_cambio
-;call arriba_i
-call plataforma
-jmp final
+jmp inicio
+
+inicio:
+escribe barra,len3
+jmp leer_tecla
 ;-----------------------------------------------------------------------------
+
+
+leer_tecla:
+
+mov eax, 3
+mov ebx, 1
+mov ecx, tecla
+mov edx, 1
+int 0x80
+;call limpiar_pantalla
+;escribe tecla,1
+;call delay 
+jmp contadores_tecla
+;call delay
+;call limpiar_pantalla
+;escribe tecla,1
+;call delay
+contadores_tecla:
+
+mov r13,0x74
+mov r14,qword[tecla]
+cmp r14,r13
+je aumento_derecha
+mov r13,0x72
+mov r14,qword[tecla]
+cmp r14,r13
+je aumento_izquierda
+jmp plataforma
+
+aumento_derecha:
+call limpiar_pantalla
+mov r13,0x00
+mov qword[tecla],r13
+mov r13,0x01
+add qword [tecla_derecha], r13
+jmp plataforma 
+
+aumento_izquierda:
+call limpiar_pantalla
+mov r13,0x00
+mov qword[tecla],r13
+mov r13,0x01
+add qword [tecla_izquierda], r13
+jmp plataforma 
+
+
 
 
 rara:
@@ -81,54 +307,17 @@ mov [barra + 0x0E], r8
 mov r8, 0x2a;*
 mov [barra + 0x0F], r8
 escribe barra, len3
-call delay
-call limpiar_pantalla
+;call delay
+;call limpiar_pantalla
 ret
 ;-----------------------------------------------------------------------------
 
-;--------------------Lectura del Teclado-----------------------------------
-Escuchar:
-	mov ah,0x06			;Peticion directa de la consola
-	mov dl,0x0ff		;Entrada del teclado
-	int 0x21			;Interrupcion
-	ret 				;Si ZF esta en alto significa que el buffer esta vacio
-;-----------------------------------------------------------------------------
 
-;-------------------Entender lo que dice el teclado---------------------------
-Atender:
-	cmp al,75	;Flecha izquierda
-	jnz E2
-	;call moverizquierda
-	;jmp principal
-E2:
-	cmp al,77	;Flecha derecha
-	jnz E3
-	;call Mover derecha
-	;jmp principal
-E3:
-	cmp al,44 	;Precionar X
-	jnz E4	;Seria el principal
-	;call Iniciar_Juego		;Parte en la que el juego comienza
-	;jmp principal
-E4:
-	cmp al,30		;Presionar a
-	jnz E5
-	;call moverizquierda
-	;jmp principal
-E5:
-	cmp al,32	;Presionar D
-	jnz Quit
-	;call Mover derecha
-	;jmp principal
-Quit:
-	cmp al,16
-	;jnz ;principal
-	ret
-;-----------------------------------------------------------------------------
+
 
 ;---------------------------retardo temporal----------------------------------
 delay:
-mov dword [tv_sec], 0
+mov dword [tv_sec], 1
 mov dword [tv_usec], 9000000
 mov eax, 162
 mov ebx, timeval
@@ -153,12 +342,18 @@ ret
 
 ;---------------------------plataforma----------------------------------
 plataforma:
-;jmp plataforma_derecha
-jmp plataforma_izquierda
-ret
+mov r13,0x00
+cmp qword[tecla_derecha],r13
+jne plataforma_derecha
+cmp qword[tecla_izquierda],r13
+jne plataforma_izquierda
+escribe barra,len3
+jmp inicio 
 ;-----------------------------------------------------------------------------
 
 plataforma_derecha:
+mov r13,0x01
+sub qword[tecla_derecha],r13
 mov r13, 0x37
 cmp qword [p1], r13
 je plataforma_limite_derecho
@@ -167,7 +362,7 @@ jmp plataforma_suma_unidad
 plataforma_limite_derecho:
 mov r13, 0x32
 cmp qword [p2], r13
-je plataforma_izquierda
+je inicio
 jmp plataforma_suma_unidad
 
 plataforma_suma_unidad:
@@ -178,7 +373,7 @@ je  plataforma_suma_decena
 mov r13,0x01
 add qword [p2], r13
 call barra_cambio
-jmp plataforma_derecha
+jmp inicio
 
 plataforma_suma_decena:
 mov r13,0x09
@@ -188,11 +383,13 @@ sub qword [p2], r13
 
 call barra_cambio
 
-jmp plataforma_derecha
+jmp inicio
 
 ;_____________________________________________________________________________
 
 plataforma_izquierda:
+mov r13,0x01
+sub qword[tecla_izquierda],r13
 mov r13, 0x30
 cmp qword [p1], r13
 je plataforma_limite_izquierdo
@@ -201,7 +398,7 @@ jmp plataforma_resta_unidad
 plataforma_limite_izquierdo:
 mov r13, 0x30
 cmp qword [p2], r13
-je plataforma_derecha
+je inicio
 jmp plataforma_resta_unidad
 
 plataforma_resta_unidad:
@@ -212,7 +409,7 @@ je  plataforma_resta_decena
 mov r13,0x01
 sub qword [p2], r13
 call barra_cambio
-jmp plataforma_izquierda
+jmp inicio
 
 plataforma_resta_decena:
 mov r13,0x09
@@ -222,10 +419,12 @@ add qword [p2], r13
 
 call barra_cambio
 
-jmp plataforma_izquierda
+jmp inicio 
 
 ;---------------------------limpia registros----------------------------------
 final:
+call canonical_on
+call echo_on
 mov eax, 1
 ;se carga la llamada 60d (sys_exit) en rax
 mov ebx,0
